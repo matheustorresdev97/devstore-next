6d748a273498e92ec3432f037e925cbdbb50cf50
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-08-19 10:28:15 -0300

    utilizando context do carrinho

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Metadata } from "next";
 import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
+import { AddToCartButton } from "@/components/add-to-cart-button";
 
 interface ProductProps {
   params: {
@@ -72,6 +73,7 @@ export default async function ProductPage({ params }: ProductProps) {
             })}
           </span>
           <span className="text-sm text-zinc-400">
+            Em até 12x s/ juros de{" "}
             {(product.price / 12).toLocaleString("pt-BR", {
               style: "currency",
               currency: "BRL",
@@ -110,12 +112,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   );
