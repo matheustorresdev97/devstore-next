341fd74475d0ad81dcd7d505bbd122b1786ff984
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-08-19 11:00:42 -0300

    criando fluxo de ci/cd

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,9 @@
-import Link from "next/link";
-import { Search } from "lucide-react";
-import Image from "next/image";
-import { CartWidget } from "./cart-widget";
-import { SearchForm } from "./search-form";
+import Link from 'next/link'
+import Image from 'next/image'
+import { Suspense } from 'react'
+
+import { CartWidget } from './cart-widget'
+import { SearchForm } from './search-form'
 
 export function Header() {
   return (
@@ -11,7 +12,10 @@ export function Header() {
         <Link href="/" className="text-2xl font-extrabold text-white">
           devstore
         </Link>
-        <SearchForm />
+
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className="flex items-center gap-4">
@@ -22,7 +26,7 @@ export function Header() {
         <Link href="/" className="flex items-center gap-2 hover:underline">
           <span className="text-sm">Account</span>
           <Image
-            src="https://github.com/matheustorresdev97.png"
+            src="https://github.com/diego3g.png"
             className="h-6 w-6 rounded-full"
             width={24}
             height={24}
@@ -31,5 +35,5 @@ export function Header() {
         </Link>
       </div>
     </div>
-  );
+  )
 }
